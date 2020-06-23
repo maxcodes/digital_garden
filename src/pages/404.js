7e27bccf39330f1f667363734b9008ef5b51938f
Max Holzheu <maxhm10@gmail.com>
2020-06-23 18:31:53 -0500

    changelog

--- src/pages/404.js
+++ src/pages/404.js
@@ -37,7 +37,7 @@ const notFound = () => {
             fill="currentColor"
           />
         </svg>
-        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
+        <p>This page doesn't exist. Maybe it did before, but now you are lost. Let's get you back <Link to="/">home</Link> safely.</p>
       </div>
     </Layout>
   )
