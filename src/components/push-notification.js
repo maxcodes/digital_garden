7862021c9367835c6b26ae30f9b7970e1e935b49
Max Holzheu <maxhm10@gmail.com>
2021-05-05 22:25:46 -0600

    use spontit v3

--- src/components/push-notification.js
+++ src/components/push-notification.js
@@ -8,19 +8,23 @@ const PushNotification = () => {
     return null;
 
   const payload = {
-    secretUser: "max",
-    userId: "max",
-    secretKey:
-      "G1M5OXZ7NWWK9OZKO1TQBMJGPH3O2JHAUX6Q26TDFH12ZWGZE4B2T4N27WDGN53ZS4R87LFFVS33B6HF30XPZN4YT6UBGFPI5TVD",
-    callToAction: `New garden-curious visitor! ${window.location.pathname}`
+    pushContent: `New garden-curious visitor! ${window.location.pathname}`
   };
 
   const stringified = JSON.stringify(payload);
   const xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST", "https://api.spontit.com/v2/push");
+  xmlhttp.open("POST", "https://api.spontit.com/v3/push");
   xmlhttp.setRequestHeader(
     "Content-Type",
     "application/x-www-form-urlencoded;charset=UTF-8"
+  );
+  xmlhttp.setRequestHeader(
+    "X-Authorization",
+    "G1M5OXZ7NWWK9OZKO1TQBMJGPH3O2JHAUX6Q26TDFH12ZWGZE4B2T4N27WDGN53ZS4R87LFFVS33B6HF30XPZN4YT6UBGFPI5TVD",
+  );
+  xmlhttp.setRequestHeader(
+    "X-UserId",
+    "jovenmaiz",
   );
   xmlhttp.send(stringified);
 
