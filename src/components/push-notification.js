40b74f5b8366392b18650e8ad4f762762a7d3172
Max Holzheu <maxhm10@gmail.com>
2021-06-10 00:47:51 -0600

    new api service for push notifications

--- src/components/push-notification.js
+++ src/components/push-notification.js
@@ -7,25 +7,15 @@ const pushNotification = (content = null) => {
   )
     return null;
 
-  const payload = {
-    pushContent: content || `New garden-curious visitor! ${window.location.pathname}`
-  };
+  const message = content || `New garden-curious visitor! ${window.location.pathname}`
 
-  const stringified = JSON.stringify(payload);
+  const formData = new FormData();
+  formData.append("token", "aemcmnw4j9zgtctjnjwyk8yuqkbhtq");
+  formData.append("user", "utzmzf1gnfymdrmrw3exk9i77r6k3m");
+  formData.append("message", message);
+
   const xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST", "https://api.spontit.com/v3/push");
-  xmlhttp.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded;charset=UTF-8"
-  );
-  xmlhttp.setRequestHeader(
-    "X-Authorization",
-    "G1M5OXZ7NWWK9OZKO1TQBMJGPH3O2JHAUX6Q26TDFH12ZWGZE4B2T4N27WDGN53ZS4R87LFFVS33B6HF30XPZN4YT6UBGFPI5TVD",
-  );
-  xmlhttp.setRequestHeader(
-    "X-UserId",
-    "jovenmaiz",
-  );
-  xmlhttp.send(stringified);
+  xmlhttp.open("POST", "https://api.pushover.net/1/messages.json");
+  xmlhttp.send(formData);
 };
 export default pushNotification;
