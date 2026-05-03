8b11e3dcef92fbb384e8ff9d438474c2158b92ea
Max Holzheu <maxhm10@gmail.com>
2026-05-03 11:59:51 -0600

    Disable stale offline service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,22 @@
 import "./src/styles/global.scss";
 import "./src/styles/bootstrap.scss";
+
+export const onClientEntry = () => {
+  if (typeof window === "undefined" || !("serviceWorker" in navigator)) return;
+
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .getRegistrations()
+      .then(registrations =>
+        Promise.all(registrations.map(registration => registration.unregister()))
+      )
+      .catch(() => {});
+
+    if (window.caches) {
+      caches
+        .keys()
+        .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+        .catch(() => {});
+    }
+  });
+};
